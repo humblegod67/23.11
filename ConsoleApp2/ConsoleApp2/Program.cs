4a8e2e954952105dd6d66faeb60f3238c8aa7f8d
humblegod67 <151152304+humblegod67@users.noreply.github.com>
2023-11-23 13:37:03 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод числа от пользователя
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // Проверка критериев
+        if (number % 5 == 2 || number % 7 == 1)
+        {
+            Console.WriteLine($"{number} удовлетворяет хотя бы одному из условий.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} не удовлетворяет ни одному из условий.");
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number % 4 == 0 && number >= 10)
+        {
+            Console.WriteLine($"{number} удовлетворяет условиям.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} не удовлетворяет условиям.");
+        }
+    }
+}
