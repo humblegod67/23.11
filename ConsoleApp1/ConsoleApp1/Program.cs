c13ebacfce186451848795aa48bfc8597a718f11
humblegod67 <151152304+humblegod67@users.noreply.github.com>
2023-11-23 13:36:01 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод числа от пользователя
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // Проверка деления на 3
+        if (number % 3 == 0)
+        {
+            Console.WriteLine($"{number} делится на 3.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} не делится на 3.");
+        }
+    }
+}
